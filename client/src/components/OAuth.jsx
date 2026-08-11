a5c0688ea2c775c15ce7d53ce5ba05f73f13f64e
Saksham <vatssakshu@gmail.com>
2026-08-12 02:33:48 +0530

    created OAuth Functionality

--- client/src/components/OAuth.jsx
+++ client/src/components/OAuth.jsx
@@ -1,4 +1,3 @@
-import React from 'react'
 import { getAuth, GoogleAuthProvider, signInWithPopup } from 'firebase/auth';
 import { app } from '../firebase';
 import { useDispatch } from 'react-redux';
