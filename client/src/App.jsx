5155a0eeed2a58b59b12cf5012e1fef3279f4a37
Saksham <vatssakshu@gmail.com>
2026-08-18 00:26:42 +0530

    Created Firebase based Image Upload functionality

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Route, Routes, BrowserRouter } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import Home from './pages/Home'
 import About from './pages/About'
 import SignIn from './pages/SignIn'
